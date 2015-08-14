5bf9410151be827dd4a00b3b2792161e0b0f2eea
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-08-14 10:37:09 -0400

    Basic presenter with proxy

--- src/main/java/com/arcbees/client/application/home/RootPresenter.java
+++ src/main/java/com/arcbees/client/application/home/RootPresenter.java
@@ -2,28 +2,30 @@ package com.arcbees.client.application.home;
 
 import javax.inject.Inject;
 
-import com.arcbees.client.place.NameTokens;
 import com.arcbees.client.widgets.computer.ComputerPresenter;
 import com.arcbees.client.widgets.computer.WidgetsFactory;
 import com.arcbees.client.widgets.hacker.HackerPresenter;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.UiHandlers;
 import com.gwtplatform.mvp.client.View;
-import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
-import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import com.gwtplatform.mvp.client.presenter.slots.NestedSlot;
+import com.gwtplatform.mvp.client.proxy.Proxy;
 
 public class RootPresenter extends Presenter<RootPresenter.MyView, RootPresenter.MyProxy> {
-    interface MyView extends View {
+    interface MyUiHandlers extends UiHandlers {
+    }
+
+    interface MyView extends View, MyUiHandlers {
     }
 
     @ProxyStandard
-    @NameToken(NameTokens.home)
-    interface MyProxy extends ProxyPlace<RootPresenter> {
+    interface MyProxy extends Proxy<RootPresenter> {
     }
 
-    public static final Object SLOT_COMPUTERS = new Object();
-    public static final Object SLOT_HACKERS = new Object();
+    public static final NestedSlot SLOT_COMPUTERS = new NestedSlot();
+    public static final NestedSlot SLOT_HACKERS = new NestedSlot();
 
     private final WidgetsFactory widgetsFactory;
 
@@ -44,7 +46,7 @@ public class RootPresenter extends Presenter<RootPresenter.MyView, RootPresenter
 
         HackerPresenter zeroCool = widgetsFactory.createHacker("Zer0C00L");
         HackerPresenter acidBurn = widgetsFactory.createHacker("AcidBurn");
-
+        zeroCool.unbind();
         addToSlot(SLOT_HACKERS, zeroCool);
         addToSlot(SLOT_HACKERS, acidBurn);
 
@@ -56,5 +58,15 @@ public class RootPresenter extends Presenter<RootPresenter.MyView, RootPresenter
 
         addToSlot(SLOT_COMPUTERS, computerA);
         addToSlot(SLOT_COMPUTERS, computerB);
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+    }
+
+    @Override
+    protected void onReveal() {
+        super.onReveal();
     }
 }
--- src/main/java/com/arcbees/client/place/NameTokens.java
+++ src/main/java/com/arcbees/client/place/NameTokens.java
@@ -1,6 +1,6 @@
 package com.arcbees.client.place;
 
 public class NameTokens {
-    public static final String home = "!home";
+    public static final String home = "/home2";
     public static final String login = "login";
 }
